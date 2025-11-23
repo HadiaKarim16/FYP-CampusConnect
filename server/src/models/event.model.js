c209bbc00570382da748215b6712904a43daf86f
Irfan Saeed <irfansaeed.narejo@gmail.com>
2025-11-23 16:57:34 +0500

    Event Model added and made some minor changes in user model too

--- server/src/models/event.model.js
+++ server/src/models/event.model.js
@@ -2,7 +2,7 @@ import mongose from "mongoose";
 
 
 
-const EventSchema = new mongose.Schema({ 
+const eventSchema = new Schema({ 
      
     title: { 
       type: String,
@@ -21,7 +21,15 @@ const EventSchema = new mongose.Schema({
       maxlength: [400, 'Description must be at most 400 characters long'],
     },
 
+    registeredStudents: [
+        {
+            type: mongose.Schema.Types.ObjectId,
+            ref: 'User',
+        }
+    ],
+    
     startAt: { type: Date, required: true },
+    endAt:{ type: Date, required: true },
 
     societyId: { 
        tpye: Schema.Types.ObjectId,
@@ -33,34 +41,64 @@ const EventSchema = new mongose.Schema({
         ref: 'Campus'
     },
 
+    location: {
+        type: {
+            type: String,
+            enum: ['online','physical'], 
+            default: 'physical',
+            required: true,
+        },
+        tag:[
+          {
+          type: String,
+          required: true,
+          index: true
+         }
+        ],
+        address: {
+            type:String,
+            required:true,
+
+        },
+
+        onlineUrl: String,   
+    },
+
     category: {
         type: String,
         enum: ['Academic', 'Cultural', 'Sports', 'Social', 'Other'],
         default: 'Other',
     },
 
-    endAt: Date,
+    
 
     capacity: {
          type: Number,
          default: 0 
         }, 
 
+    requireApproval: { 
+        type: Boolean, 
+        default: false 
+    },
     isCanceled: { 
         type: Boolean,
         default: false
      },
-    
-    tagsCount: { type: Number, default: 0 }, 
+     
     registrationsCount: { type: Number, default: 0 },
-    ratingAvg: { type: Number, default: 0 }, 
 
-
-
+    
 
 },
  
 {timestamps: true});
 
+eventSchema.index({ campusId: 1, startAt: 1 });
+eventSchema.index({ societyId: 1, startAt: 1 });
+eventSchema.index({ _id: 1, startAt: 1 });
+eventSchema.index({ _id: 1, registeredStudents: 1 });
+eventSchema.index({ title: 'text', description: 'text' });
+eventSchema.index({ isCanceled: 1 }, { partialFilterExpression: { isCanceled: { $eq: false } } });
 
-export const Event = mongose.model("Event", EventSchema);
+export const Event = mongose.model("Event", eventSchema);
