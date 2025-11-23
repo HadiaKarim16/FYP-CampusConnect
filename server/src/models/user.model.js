6d38933b46007a7b5d2cbddcaf0f4818d03f71a6
Irfan Saeed <irfansaeed.narejo@gmail.com>
2025-11-23 15:42:50 +0500

    Add user model with schema definition for user data

---
+++ server/src/models/user.model.js
@@ -0,0 +1,156 @@
+import mongoose, { SchemaTypes } from "mongoose";
+
+
+
+const userSchema = new mongoose.Schema( {
+		
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true,
+        validate: {
+            validator: function(v) {
+                return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid education email!`
+        }
+
+    },
+
+    emailVerified: { 
+        type: Boolean, 
+        default: false 
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    }, 
+    roles: [
+    { 
+        type: String,
+        enum: ['student','mentor','society_head','admin'], 
+        default: ['student']
+     }
+    ],
+
+    campusId:
+     { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Campus' 
+    },
+
+    profile: {
+
+    firstName: {
+        type: String,
+        required: [true, 'First name is required'],
+        trim: true,
+        lowercase: true,
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Last name is required'],
+        trim: true,
+        lowercase: true,
+    },
+    displayName: {
+        type: String,
+        required: [true, 'Display name is required'],
+        trim: true,
+        unique: true,
+        lowercase: true,
+        index: true,
+        validate: {
+            validator: function(v) {
+                return /^[a-z0-9_]+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid display name!`
+        },
+
+    },
+    avatarUrl: {
+        type: String, //cloudinary url
+        required: true,
+    },
+    coverImage: {
+        type: String, //cloudinary url
+    },
+    bio: {
+        type:String,
+        maxlength: 300,
+        default: '',
+        trim: true,
+    },
+
+    phone: {
+        type: String,
+        default: '',
+        trim: true,
+        validate: {
+            validator: function(v) {
+                return /^\+92(3[0-9]{9})$/.test(v);
+
+            },
+            message: props => `${props.value} is not a valid phone number!`
+    }},
+
+
+    degree: {
+        type: String,
+        default: '',
+        trim: true,
+    },
+
+    department: {
+        tpye: String,
+        default: '',
+        trim: true,
+    },
+
+    semester: {
+        type: Number,
+        default: 0,
+        min: 1,
+        max: 8
+    },
+    },
+
+    interests: [
+        
+        { tpye : String}
+    ], 
+
+    socialLinks: [
+    { 
+        provider: { type: String },
+        url: { type: String}
+    }
+    ],
+
+
+    onboarding: {
+    completedSteps: [String],
+    roleSelected: String,
+    createdAt: { type: Date, default: Date.now }
+  },
+
+    verification: {
+    isVerifiedMentor: { type: Boolean,
+    default: false 
+  }},
+    
+    status: { type: String, enum: ['active','suspended','deleted'], default: 'active' },
+
+    refreshToken: {
+        type: String
+    }
+  },
+  { timestamps: true });
+
+UserSchema.index({ campusId: 1, roles: 1 });
+
+
+export const User = mongoose.model("User", userSchema);
