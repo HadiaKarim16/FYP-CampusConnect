0164888a0d4391e33af0bc52d85f663be3e48da2
Irfan Saeed <irfansaeed.narejo@gmail.com>
2025-11-24 17:01:06 +0500

    Implement authentication and error handling middlewares; add utility classes for API responses and async handling

--- server/src/middlewares/openAuth.middleware.js
+++ server/src/middlewares/openAuth.middleware.js
@@ -3,22 +3,20 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
-export const verifyJWT = asyncHandler(async (req, _, next) => {
+export const checkUser = asyncHandler(async (req, _, next) => {
     try {
         const token =
             // req.cookies?.accessToken ||
             req.header("Authorization")?.replace("Bearer ", "");
 
-        if (!token) {
-            throw new ApiError(401, "Unauthorized request");
-        }
+        if (token) {
+            const decodedToken = jwt.verify(
+                token,
+                process.env.ACCESS_TOKEN_SECRET
+            );
 
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, decodedToken) => {
-            if (err) {
-                if (err.name === 'TokenExpiredError') {
-                    return next(new ApiError(401, "TokenExpiredError"));
-                }
-                return next(new ApiError(401, "Invalid access token"));
+            if (!decodedToken) {
+                next();
             }
 
             const user = await User.findById(decodedToken?._id).select(
@@ -26,12 +24,12 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
             );
 
             if (!user) {
-                throw new ApiError(401, "Invalid Access Token");
+                next();
             }
 
             req.user = user;
-            next();
-        });
+        }
+        next();
     } catch (error) {
         throw new ApiError(401, error?.message || "Invalid access token");
     }
